1bc98df353f682914632d83a69f1db2ca5c534c3
pengu <bortiznine@gmail.com>
2022-11-20 12:53:05 -0600

    Added selects for 5-8 queries. -BMO

--- src/MyMainClass.java
+++ src/MyMainClass.java
@@ -22,15 +22,15 @@ public class MyMainClass {
                     break;
                 case 2:
                     System.out.println(
-                            "Query1(1):" +
-                            "\nQuery2(2):" +
-                            "\nQuery3(3)" +
-                            "\nQuery4(4)" +
-                            "\nQuery3(5)" +
-                            "\nQuery3(6)" +
-                            "\nQuery3(7)" +
-                            "\nQuery3(8)" +
-                            "\nReturn(0)");
+                            "Query1 (1):" +
+                            "\nQuery2 (2):" +
+                            "\nQuery3 (3)" +
+                            "\nQuery4 (4)" +
+                            "\nGet a count of all customers by name that only ordered seasonal products and dine at Baker’s Square. (5)" +
+                            "\nDisplay the names and birthdays of customers in descending order that ordered Cobb salad at Olive Garden. (6)" +
+                            "\nShow supply requests by date and restaurant name of all restaurants that ordered on US holidays in 2020. (7)" +
+                            "\nIdentify customers by name, and date of birth that have no dine orders but have deliveries. (8)" +
+                            "\nReturn (0)");
                     Scanner sc2 = new Scanner(System.in);
                     System.out.println("Enter a value :");
                     int input2 = sc2.nextInt();
@@ -63,12 +63,65 @@ public class MyMainClass {
                             case 4:
                                 break;
                             case 5:
+                                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/FoodServices", "root", "AcabJet1509$");
+                                System.out.println("Connected");
+                                statement = con.createStatement();
+                                PreparedStatement p =con.prepareStatement("SELECT COUNT( C.Cname) FROM Customer AS C, Orders AS O, Grocery_Store AS G, Item AS GI, Dines AS D, Restaurant AS R WHERE C.Custid=O.Custid " +
+                                        "AND G.Gid=O.Gid AND G.Gid=GI.Gid AND GI.Item_Name='seasonal products'" +
+                                        " AND C.Custid = D.Custid AND R.Rid = D.Rid AND R.Rname=?; ");
+                                p.setString(1, "Sunny's Steakhouse");
+                                ResultSet query=p.executeQuery();
+                                while (query.next()) {
+                                    // print the results
+                                    System.out.println(query.getInt("COUNT(C.name)"));
+                                }
                                 break;
                             case 6:
+                                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/FoodServices", "root", "AcabJet1509$");
+                                System.out.println("Connected");
+                                statement = con.createStatement();
+                                ResultSet rs6 = statement.executeQuery("SELECT C.Cname, C.DateOfBirth FROM Customer AS C, Dines AS D, Restaurant AS R, Dish AS DI " +
+                                        "WHERE C.Custid = D.Custid AND R.Rid = D.Rid AND R.Rid = DI.Rid AND DI.Dish_Name = 'Cobb salad' ORDER BY Cname DESC, C.DateOfBirth DESC ;");
+                                statement.close();
+                                while (rs6.next()) {
+                                    String cName = rs6.getString("C.Cname");
+                                    Date dob = rs6.getDate("C.DateofBirth");
+
+
+                                    // print the results
+                                    System.out.format("%s, %s\n", cName, dob);
+                                }
                                 break;
                             case 7:
+                                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/FoodServices", "root", "AcabJet1509$");
+                                System.out.println("Connected");
+                                statement = con.createStatement();
+                                ResultSet rs7 = statement.executeQuery("SELECT S.Supply_Date,R.Rname FROM Supplies AS S, Restaurant AS R WHERE S.Rid=R.Rid AND DATE(Supply_Date) " +
+                                        "in ('2020-01-01', '2020-01-17','2020-02-12','2020-02-14','2020-02/21','2020-03-17','2020-04-13','2020-05-30', '2020-07-04', '2020-09-05', '2020-11-11','2020-11-24','2020-12-25'); ");
+                                statement.close();
+                                while (rs7.next()) {
+                                    Date supply_date = rs7.getDate("S.Supply_Date");
+                                    String rname = rs7.getString("R.Rname");
+
+
+                                    // print the results
+                                    System.out.format("%s, %s\n", supply_date, rname);
+                                }
                                 break;
                             case 8:
+                                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/FoodServices", "root", "AcabJet1509$");
+                                System.out.println("Connected");
+                                statement = con.createStatement();
+                                ResultSet rs8 = statement.executeQuery("SELECT C.Cname, C.DateOfBirth FROM Customer AS C, Delivers AS D1, Dines AS D2 WHERE C.Custid=D1.Custid and C.Custid!=D2.Custid; ");
+                                statement.close();
+                                while (rs8.next()) {
+                                    String cName = rs8.getString("C.Cname");
+                                    Date dob = rs8.getDate("C.DateOfBirth");
+
+
+                                    // print the results
+                                    System.out.format("%s, %s\n", cName, dob);
+                                }
                                 break;
                             case 0:
                                 System.out.println("Returning to Main Menu!");
