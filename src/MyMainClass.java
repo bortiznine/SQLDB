e0818d18a28b31d2cb3c8cdbdfb09d54733c679c
patri <patrick.poinar@qubuilder.com>
2022-11-22 17:23:53 -0600

    Implement: case 4

--- src/MyMainClass.java
+++ src/MyMainClass.java
@@ -12,7 +12,7 @@ public class MyMainClass {
 
         int input=3;
         while (input!=0) {
-            System.out.println("Welcome to the Food Service DB");
+            System.out.println("\nWelcome to the Food Service DB");
             System.out.println("Are you a entering a new Customer(1):\nLooking up information(2):\nExit (0)");
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter a value :");
@@ -72,7 +72,7 @@ catch(Exception e){
                             "Display the customers (by address and birthday) that ordered soda between 2010 and 2020. (1)" +
                             "\nDisplay in alphabetical order the names, addresses, and the average number of orders from restaurants that get supplied with white bread. (2)" +
                             "\nDisplay all Grocery stores and their addresses that have margarine and have supply request in 2022 (3)" +
-                            "\nQuery4 (4)" +
+                            "\nDisplay top 5 Grocery stores by orders (4)" +
                             "\nGet a count of all customers by name that only ordered seasonal products and dine at Baker’s Square. (5)" +
                             "\nDisplay the names and birthdays of customers in descending order that ordered Cobb salad at Olive Garden. (6)" +
                             "\nShow supply requests by date and restaurant name of all restaurants that ordered on US holidays in 2020. (7)" +
@@ -92,7 +92,7 @@ catch(Exception e){
                                 ResultSet rs1 = statement.executeQuery("SELECT     C.Cname, C.DateOfBirth\n" +
                                         "FROM        Customer AS C, Orders AS O, Grocery_Store AS G\n" +
                                         "WHERE        G.Gid in (select G.Gid from Grocery_Store as G,Item as GI where G.Gid=GI.Gid and GI.item_Name='soda') AND (O.Order_Date BETWEEN '2010-01-01' AND '2020-12-31') AND (C.Custid = O.Custid AND O.Gid = G.Gid );\n;");
-
+                                System.out.println("Customer Name | Date of Birth\n");
                                 while (rs1.next()) {
                                     String Cname = rs1.getString("Cname");
                                     Date DateOfBirth = rs1.getDate("DateOfBirth");
@@ -112,6 +112,7 @@ catch(Exception e){
                                         "FROM Restaurant AS R, Supplies AS S, Grocery_Store AS G, Item AS GI  \n" +
                                         "WHERE (GI.Item_Name= 'white bread') AND (R.Rid = S.Rid AND S.Gid = G.Gid AND G.Gid = GI.Gid) \n" +
                                         "ORDER BY R.Rname , R.Address ASC;   ");
+                                System.out.println("Restaurant Name | Address | Average Orders\n");
                                 while (rs2.next()) {
 
                                     String rname = rs2.getString("R.Rname");
@@ -132,7 +133,7 @@ catch(Exception e){
                                 statement = con.createStatement();
                                 ResultSet rs3 = statement.executeQuery(" SELECT G.Gname, G.Address  FROM Grocery_Store AS G, Supplies AS S,Item AS GI\n" +
                                         "WHERE(GI.Item_Name = 'margarine') AND YEAR(S.Supply_Date) = 2019 AND (G.Gid = S.Gid  AND G.Gid = GI.Gid);   ");
-                                System.out.println("Customer Name | Date of Birth\n");
+                                System.out.println("Customer Name | Address\n");
                                 while (rs3.next()) {
                                     String Gname = rs3.getString("G.Gname");
                                     String Gaddress = rs3.getString("G.Address");
@@ -146,6 +147,21 @@ catch(Exception e){
                                 Thread.sleep(3000);
                                 break;
                             case 4:
+                                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/FoodServices", "root", "AcabJet1509$");
+                                System.out.println("Connected");
+                                statement = con.createStatement();
+                                ResultSet rs4 = statement.executeQuery(" SELECT G.Gname FROM Grocery_Store AS G, Orders AS O \n" +
+                                        "WHERE G.Gid = O.Gid GROUP BY G.Gname Order BY Count(O.Oid) DESC LIMIT 5;   ");
+                                System.out.println("Grocery Store Name\n");
+                                while (rs4.next()) {
+                                    String Gname = rs4.getString("G.Gname");
+
+                                    // print the results
+                                    System.out.format("\n %s ", Gname );
+                                }
+                                statement.close();
+                                con.close();
+                                Thread.sleep(3000);
                                 break;
                             case 5:
                                 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/FoodServices", "root", "AcabJet1509$");
